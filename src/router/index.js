68b7c65a3edbf75c4bdfd2a7b11923bdbb3067df
shuaishuai <372823776@qq.com>
2018-07-09 11:59:58 +0800

    base model

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,19 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+/* 
+  组件的引入 方式一 require
+*/
+//   const login = r => require.ensure([], () => r(require('@/page/login2')), 'login');
+/* 
+  组件的引入 方式二 import
+*/
+// export const constantRouterMap = [
+//   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+// ]
+
 export default new Router({
-  routes: [
-    {
-      // path: '/',
-      // name: 'HelloWorld',
-      // component: HelloWorld
-    }
-  ]
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  // routes:constantRouterMap
 })
